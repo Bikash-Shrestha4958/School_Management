c18f179fcd49a50470759f07fe6954e5a3394979
Bikash-Shrestha4958 <chester4958@gmail.com>
2022-08-30 08:55:13 +0545

    .

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
 <footer class="main">
-	&copy; <?php echo date('Y')?> | School Management System in PHP using CodeIgniter Web Framework |
-    <strong>Developed by Md Riadul Islam, TMSS ICT</strong>
+	&copy; <?php echo date('Y')?> | School Management System in PHP 
+    <strong></strong>
 </footer>
